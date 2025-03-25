d0e5fea726fbd416affae550a0e4caf428f838c4
Sudhanshu Dasgupta <dasguptashivam23@gmail.com>
2025-03-25 22:25:10 +0530

    fix edit pthis page link
    Signed-off-by: Sudhanshu Dasgupta <dasguptashivam23@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/safedep/docs.safedep.io/tree/main/docs',
+            'https://github.com/safedep/docs.safedep.io/tree/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
